eb7bf85b04fd2519ada1e14828bbb0db005b552c
Tom Payne <tpayne@fastmail.co.uk>
2020-07-22 21:50:24 +0100

    Fixed cron running of python scrip

--- dockerfile
+++ dockerfile
@@ -3,7 +3,8 @@ FROM python:3
 WORKDIR /usr/src/app
 
 COPY pi-plotter/requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --upgrade pip && \
+    pip install --no-cache-dir -r requirements.txt
 
 COPY crontab /etc/cron.d/pi-plotter
 RUN chmod 0644 /etc/cron.d/pi-plotter
--- pi-plotter/plot_temperature.py
+++ pi-plotter/plot_temperature.py
@@ -1,36 +1,10 @@
-import fileinput
-from maraxparser import createTimeBuckets, averageExchangerTemps, parseRow, extractExchangerTemps
-import numpy as np
 import matplotlib.pyplot as plt
 import io
 from PIL import Image, ImageDraw, ImageFont
 
-useBuckets = False
-lines = fileinput.input()
+print("running")
 
-if (useBuckets):
-    secondsPerBucket = 60
-
-    timeBuckets = createTimeBuckets(secondsPerBucket, lines)
-    exchangerTemps = list(map(averageExchangerTemps, timeBuckets))
-else:
-    parsedLines = list(map(parseRow, lines))
-    exchangerTemps = extractExchangerTemps(parsedLines)
-
-print(exchangerTemps)
-
-lastIndex = len(exchangerTemps) - 1
-xy = (lastIndex - 1, exchangerTemps[lastIndex])
-lastPlot = {
-    'xy': xy,
-    'title': str(round(xy[1], 1)) + '°C',
-    'xytext': (xy[0] + 1.1, xy[1] + 1.1)
-}
-
-plt.plot(exchangerTemps)
-plt.axis('off')
-plt.autoscale(False)
-plt.annotate(lastPlot['title'], xy=lastPlot['xy'], xytext=lastPlot['xytext'])
+plt.plot([1,2,3,4,5])
 
 with io.BytesIO() as file:
     plt.savefig(file)
